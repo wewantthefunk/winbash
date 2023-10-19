85fd4bb033fa69de48788fcebef214dead29e40c
Christian Strama <oakandlionsociety@gmail.com>
2023-10-18 21:32:39 -0400

    updates

--- IInput.cs
+++ IInput.cs
@@ -2,7 +2,7 @@ namespace bash.dotnet {
     interface IInput {
         void AcceptInput(IView nullView, CommandFactory factory);
         ConfigOptions ReadConfig(ConfigOptions configOptions);
-
         void SetCurrentDirectory(string currentDirectory);
+        string[] getAliases();
     }
 }
---
+++ compgen.dotnet.cs
@@ -0,0 +1,38 @@
+﻿namespace bash.dotnet {
+    internal class COMPGENBash : ICommand {
+        private IView _view;
+        private ConfigOptions _configOptions;
+        private IInput _inputDevice;
+        private ICommandFactory _factory;
+
+        public COMPGENBash (IView view, ConfigOptions configOptions, IInput inputDevice, ICommandFactory factory) {
+            _view = view;
+            _configOptions = configOptions;
+            _inputDevice = inputDevice;
+            _factory = factory;
+        }
+
+        public ConfigOptions Go(string[] args) {
+            if (args.Length == 0) {
+                _view.DisplayError("invalid switch\n");
+            }
+
+            string[] s = new string[0];
+
+            if (args[0] == "-a") {
+                s = _inputDevice.getAliases();                
+            } else if (args[0] == "-b") {
+                s = _factory.getBuiltInCommands();
+            }
+
+            _view.DisplayInfo("Total " + s.Length.ToString() + Environment.NewLine);
+            foreach (string s1 in s) {
+                _view.DisplayInfo(s1 + Environment.NewLine);
+            }
+
+            return _configOptions;
+        }
+
+        public void SetProperty(string key, string value) { }
+    }
+}
